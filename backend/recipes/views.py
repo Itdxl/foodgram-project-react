186b3dc391e1a1d017daa0c9c1a1fdae154d7711
Dmitry Shevchenko <logimor@yandex.com>
2023-04-03 14:20:40 +0300

    tag/ing v/s recr

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, status
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticatedOrReadOnly,
+)
+from .models import (
+    Ingredient,
+    Tag
+    )
 
-# Create your views here.
+from .serializers import (
+    TagSerializer,
+    IngredientSerializer
+    )
+
+
+class TagView(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permissions = [AllowAny, ]
+    pagination_class = None
+
+
+class IngredientView(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
+    serializer_class = IngredientSerializer
+    search_fields = ["name", ]
+    pagination_class = None
+    # filter_backends = [DjangoFilterBackend, ]
+    # filter_class = IngredientFilter
