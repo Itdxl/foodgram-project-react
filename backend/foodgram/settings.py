587cff78947308e2e3653e08754ddcd358d1b922
Dmitry Shevchenko <logimor@yandex.com>
2023-04-07 16:59:37 +0300

    10000

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,8 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY',
-                       default='django-insecure-6xl_ra67w6omt+q*ph#^l*berfzv#ybd3pjfqvz^zk+-d7q&v#')
+SECRET_KEY = 'django-insecure-6xl_ra67w6omt+q*ph#^l*berfzv#ybd3pjfqvz^zk+-d7q&v#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
