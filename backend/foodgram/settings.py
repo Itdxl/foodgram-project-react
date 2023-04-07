d2108feff7be2c1f867115ba8148654d7b95052a
Dmitry Shevchenko <logimor@yandex.com>
2023-04-07 11:52:29 +0300

    settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,7 +1,11 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+load_dotenv()
+
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
