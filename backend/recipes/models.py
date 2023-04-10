3affafa5efc05facb3742dcfbab28263ab8ff990
Dmitry Shevchenko <logimor@yandex.com>
2023-04-10 11:47:57 +0300

    -sh

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -183,6 +183,10 @@ class ShoppingCart(models.Model):
     )
 
     class Meta:
+        constraints = [models.UniqueConstraint(
+            fields=['user', 'recipe'],
+            name='unique_shopping_cart'
+        )]
         ordering = ('-id',)
         verbose_name = 'Список покупок'
         verbose_name_plural = 'Списки покупок'
