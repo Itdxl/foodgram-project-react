a535243ae0eb83866113bbe1dea3932334b899cc
Dmitry Shevchenko <logimor@yandex.com>
2023-04-11 11:19:41 +0300

    admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,20 +1,3 @@
 from django.contrib import admin
 
-from django.contrib.auth.models import Group, Permission
-from django.db import models
-
-# Переопределение модели Group
-class MyGroup(Group):
-    name = models.CharField(max_length=150, verbose_name="Имя группы")
-
-    class Meta:
-        verbose_name = "Группа"
-        verbose_name_plural = "Группы"
-
-# Переопределение модели Permission
-class MyPermission(Permission):
-    name = models.CharField(max_length=255, verbose_name="Имя разрешения")
-
-    class Meta:
-        verbose_name = "Разрешение"
-        verbose_name_plural = "Разрешения"
+# Register your models here.
